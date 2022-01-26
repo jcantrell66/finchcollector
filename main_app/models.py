3aff837377f1cf43c6b845f315861ca9645e49f9
Joshua Cantrell <JoshCantrellsMac@Joshuas-MacBook-Pro-4.local>
2022-01-26 17:54:17 -0500

    complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,16 +1,46 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
 
 class Bird(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'bird_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    bird = models.ForeignKey(Bird, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Bird
+from django.views.generic import ListView, DetailView
+from .models import Bird, Toy
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Welcome to FinchCollector</h1>')
+    return HttpResponse('<h1>Welcome to Bird Collector</h1>')
 
 def about(request):
     return render(request, 'about.html')
@@ -16,7 +18,27 @@ def birds_index(request):
 
 def birds_detail(request, bird_id):
     bird = Bird.objects.get(id=bird_id)
-    return render(request, 'birds/detail.html', {'bird': bird})
+    toys_bird_doesnt_have = Toy.objects.exclude(id__in = bird.toys.all().values_list('id'))
+    feeding_form = FeedingForm()
+    return render(request, 'birds/detail.html', {
+        'bird': bird, 
+        'feeding_form': feeding_form,
+        'toys': toys_bird_doesnt_have
+        })
+
+def assoc_toy(request, bird_id, toy_id):
+    Bird.objects.get(id=bird_id).toys.add(toy_id)
+    return redirect('detail', bird_id=bird_id)
+
+
+
+def add_feeding(request, bird_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.bird_id = bird_id
+        new_feeding.save()
+    return redirect('detail', bird_id=bird_id)
 
 class BirdCreate(CreateView):
     model = Bird
@@ -30,12 +52,23 @@ class BirdDelete(DeleteView):
     model = Bird
     success_url = '/birds/'
 
-# birds = [
-#   Bird('Lolo', 'finch', 'foul little demon', 3),
-#   Bird('Sachi', 'parrot', 'diluted tortoise shell', 0),
-#   Bird('Raven', 'chickadee', '3 legged bird', 4)
-# ]
+class ToyList(ListView):
+  model = Toy
 
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
 
 
 # admin info
